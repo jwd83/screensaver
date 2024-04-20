1251445a47041db216f020518ce9b278ed825c84
Jared De Blander <jared@ayrscott.com>
2024-04-20 02:18:26 -0400

    weathermap stuffs

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import pygame
 import json
 import time
 import math
+from weather import OpenWeatherMap
 
+owm = OpenWeatherMap()
+owm.update()
 
 # load the settings.json into a settings object
-with open('settings.json') as json_file:
+with open("settings.json") as json_file:
     settings = json.load(json_file)
 
-blank_space = ' '
+blank_space = " "
 
 font_color = (255, 255, 255)
 
-if int(settings['space_doubling']) > 1:
-    blank_space = ' ' * int(settings['space_doubling'])
+if int(settings["space_doubling"]) > 1:
+    blank_space = " " * int(settings["space_doubling"])
 
 
 # start pygame
@@ -38,9 +41,9 @@ screen = pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
 pygame.display.set_caption("Binary Dragon Screen Saver")
 
 # load the font Bakemono-Stereo-Extrabold-trial.ttf
-font = pygame.font.Font(settings['font'], settings['fontSize'])
+font = pygame.font.Font(settings["font"], settings["fontSize"])
 
-small_font = pygame.font.Font(settings['font'], settings['fontSize'] // 2)
+small_font = pygame.font.Font(settings["font"], settings["fontSize"] // 2)
 
 # calcuate the size of the text we are going to render once so small variations in "fixed" size fonts don't cause the text to move around
 text_time = font.render("55:55:55", True, font_color)
@@ -57,18 +60,18 @@ width_am_pm = text_am_pm.get_width()
 height_am_pm = text_am_pm.get_height()
 
 
-
-
-
 while True:
+    # update the weather as needed
+    owm.update()
 
     # get the current unix time in seconds
     current_time = time.time()
 
     font_color = (
-        150+math.sin(current_time/2)*100, 
-        150+math.sin(current_time/3)*100, 
-        150+math.sin(current_time/4)*100)
+        150 + math.sin(current_time / 2) * 100,
+        150 + math.sin(current_time / 3) * 100,
+        150 + math.sin(current_time / 4) * 100,
+    )
 
     # fill the screen with black
 
@@ -81,42 +84,39 @@ while True:
     # %p to get AM or PM
     clock_string = time.strftime("%I:%M:%S")
     # if the first character is a zero, replace it with two spaces for this font... oof. maybe a bad font pick..
-    if clock_string[0] == '0':
+    if clock_string[0] == "0":
         clock_string = blank_space + clock_string[1:]
-
 
     # create text of a clock face like 12:34:56 AM using the font we have selected
     text_time = font.render(clock_string, True, font_color)
-
-
 
     # TENTHS OR HUNDREDTHS OF A SECOND
 
     # make the hundreds of a second the current time in milliseconds
     # and convert it to a string with a leading zero if needed
-    
+
     # first check if we want tenths instead
     tenths = str(int(time.time() * 10) % 10)
     hundredths = str(int(time.time() * 100) % 100)
 
-    if settings['tenths_or_hundredths'] == 'tenths':
+    if settings["tenths_or_hundredths"] == "tenths":
 
         # create text of the hundredths of a second from the tenths instead
         text_hundredths = small_font.render(tenths, True, font_color)
 
     else:
         if len(hundredths) == 1:
-            hundredths = '0' + hundredths
+            hundredths = "0" + hundredths
         # create text of the hundredths of a second
         text_hundredths = small_font.render(hundredths, True, font_color)
 
-    # AM/PM 
+    # AM/PM
     am_pm = time.strftime(" %p")
     text_am_pm = font.render(am_pm, True, font_color)
 
     # day of the week
     day_of_week = time.strftime("%A")
-    text_day_of_week = font.render(day_of_week, True, font_color)
+    text_day_of_week = small_font.render(day_of_week, True, font_color)
 
     # ISO week number
     week_number = time.strftime("Week %U")
@@ -124,58 +124,74 @@ while True:
 
     # day month and year
     day_month_year = time.strftime("%B %d, %Y")
-    text_day_month_year = font.render(day_month_year, True, font_color)
+    text_day_month_year = small_font.render(day_month_year, True, font_color)
 
-
+    # weather
+    text_weather = small_font.render(
+        f"{owm.weather_description} @ {int(owm.current_temperature)}°F",
+        True,
+        font_color,
+    )
 
     # DRAW TO THE SCREEN
 
     # blit the text to the screen at the center of the screen using the precautions we took earlier
-    screen.blit(text_time, ((screen_size[0] - width_time) // 2, (screen_size[1] - height_time) // 2))
-    screen.blit(text_hundredths, ((screen_size[0] - width_time) // 2 + width_time, (screen_size[1] - height_time) // 2))
-    screen.blit(text_am_pm, ((screen_size[0] - width_time) // 2 + width_time + width_hundredths, (screen_size[1] - height_time) // 2))
+    screen.blit(
+        text_time,
+        ((screen_size[0] - width_time) // 2, (screen_size[1] - height_time) // 2),
+    )
+    screen.blit(
+        text_hundredths,
+        (
+            (screen_size[0] - width_time) // 2 + width_time,
+            (screen_size[1] - height_time) // 2,
+        ),
+    )
+    screen.blit(
+        text_am_pm,
+        (
+            (screen_size[0] - width_time) // 2 + width_time + width_hundredths,
+            (screen_size[1] - height_time) // 2,
+        ),
+    )
 
     # draw the day of the week at the top left
     screen.blit(text_day_of_week, (0, 0))
 
+    # draw the day month and year directly below the day of the week
+    screen.blit(text_day_month_year, (0, height_hundredths))
+
+    # draw the weather at the bottom left
+    screen.blit(text_weather, (0, screen_size[1] - height_hundredths))
+
     # draw the week number at the top right
     screen.blit(text_week_number, (screen_size[0] - text_week_number.get_width(), 0))
-
-    # draw the day month and year at the bottom left
-    
-    screen.blit(text_day_month_year, (0, screen_size[1] - height_time))
-
-
 
     # draw the tenth's gauge
     for i in range(9):
         start_angle = i / 9 * 2 * 3.14159
-        stop_angle = (i+1) / 9 * 2 * 3.14159
-        angle_width = settings['fontSize'] // int(settings['tick_thickness_divider'])
-        draw_at =                 (
-                    (screen_size[0] - width_time) // 2 + width_time,
-                    (screen_size[1] - height_time) // 2 + height_hundredths - settings['fontSize'] // 6, 
-                    settings['fontSize'] // 3, settings['fontSize'] // 3
-                )
-
-        # default to disabled color    
-        draw_color = (50,50,50) 
-
-        # check if active
-        if int(tenths)  > i:
-            draw_color = font_color
-            # pygame.draw.arc(screen, font_color, (screen_size[0] // 2 - 50, screen_size[1] // 2 - 50, 100, 100), i * 40, (i + 1) * 40, 5)
-        # draw the segment 
-        pygame.draw.arc(
-            screen, 
-            draw_color, 
-            draw_at,
-            start_angle,
-            stop_angle,
-            angle_width
+        stop_angle = (i + 1) / 9 * 2 * 3.14159
+        angle_width = settings["fontSize"] // int(settings["tick_thickness_divider"])
+        draw_at = (
+            (screen_size[0] - width_time) // 2 + width_time,
+            (screen_size[1] - height_time) // 2
+            + height_hundredths
+            - settings["fontSize"] // 6,
+            settings["fontSize"] // 3,
+            settings["fontSize"] // 3,
         )
 
+        # default to disabled color
+        draw_color = (50, 50, 50)
 
+        # check if active
+        if int(tenths) > i:
+            draw_color = font_color
+            # pygame.draw.arc(screen, font_color, (screen_size[0] // 2 - 50, screen_size[1] // 2 - 50, 100, 100), i * 40, (i + 1) * 40, 5)
+        # draw the segment
+        pygame.draw.arc(
+            screen, draw_color, draw_at, start_angle, stop_angle, angle_width
+        )
 
     # # draw a circle divided up into 9 regions for tenths of a second. all will be dim at 0 and each segment will light up when it's tenth
     # # has passed. This will be a visual representation of the tenths of a second.
@@ -187,47 +203,40 @@ while True:
     #     angle_width = settings['fontSize'] // int(settings['tick_thickness_divider'])
     #     draw_at = (
     #         (screen_size[0] - width_time) // 2 + width_time + settings['fontSize'] // 20,
-    #         (screen_size[1] - height_time) // 2 + height_hundredths - settings['fontSize'] // 6, 
-    #         settings['fontSize'] // 4, 
+    #         (screen_size[1] - height_time) // 2 + height_hundredths - settings['fontSize'] // 6,
+    #         settings['fontSize'] // 4,
     #         settings['fontSize'] // 4
     #     )
 
-    #     # default to disabled color    
-    #     draw_color = (80, 80, 80) 
+    #     # default to disabled color
+    #     draw_color = (80, 80, 80)
 
     #     # check if active
     #     if int(hundredths)  > i:
     #         draw_color = font_color
 
-    #     # draw the segment 
+    #     # draw the segment
     #     pygame.draw.arc(
-    #         screen, 
-    #         draw_color, 
+    #         screen,
+    #         draw_color,
     #         draw_at,
     #         start_angle,
     #         stop_angle,
     #         angle_width
     #     )
 
-
-
     pygame.display.flip()
 
     # run at 60 fps
-    clock.tick(settings['fps'])
-
+    clock.tick(settings["fps"])
 
     # check for events
 
     for event in pygame.event.get():
 
         # quit for basically any reason...
-        if event.type in [pygame.QUIT, pygame.KEYDOWN, pygame.MOUSEBUTTONDOWN ]:
+        if event.type in [pygame.QUIT, pygame.KEYDOWN, pygame.MOUSEBUTTONDOWN]:
 
             pygame.quit()
 
             exit()
-
-
-
-
--- weather.py
+++ weather.py
@@ -1,11 +1,103 @@
 import json
+import requests
+import time
+
+
+class OpenWeatherMap:
+    def __init__(self):
+
+        # load the settings.json into a settings object
+        with open("settings.json") as json_file:
+            settings = json.load(json_file)
+
+        lat = settings["openweathermap_lat"]
+        lon = settings["openweathermap_lon"]
+        key = settings["openweathermap_api_key"]
+
+        self.lat = lat
+        self.lon = lon
+        self.key = key
+        self.update_interval = float(settings["openweathermap_update_minutes"]) * 60
+        self.last_update = 0
+
+        self.current_temperature = 0
+        self.current_pressure = 0
+        self.current_humidity = 0
+        self.weather_description = ""
+
+    def update(self):
+
+        if time.time() - self.last_update < self.update_interval:
+            print(
+                "Skipping update for another "
+                + str(self.update_interval - (time.time() - self.last_update))
+                + " seconds."
+            )
+            return
+        else:
+            print("Updating")
+
+        self.last_update = time.time()
+
+        lat = self.lat
+        lon = self.lon
+        key = self.key
+
+        complete_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}"
+        response = requests.get(complete_url)
+        x = response.json()
+
+        if x["cod"] != "404":
+
+            # store the value of "main"
+            # key in variable y
+            y = x["main"]
+
+            # store the value corresponding
+            # to the "temp" key of y
+            self.current_temperature = self.kelvin_to_fahrenheit(y["temp"])
+
+            # store the value corresponding
+            # to the "pressure" key of y
+            self.current_pressure = y["pressure"]
+
+            # store the value corresponding
+            # to the "humidity" key of y
+            self.current_humidity = y["humidity"]
+
+            # store the value of "weather"
+            # key in variable z
+            z = x["weather"]
+
+            # store the value corresponding
+            # to the "description" key at
+            # the 0th index of z
+            self.weather_description = z[0]["description"]
+
+            # print following values
+            print(
+                " Temperature (in fahrenheit unit) = "
+                + str(self.current_temperature)
+                + "\n atmospheric pressure (in hPa unit) = "
+                + str(self.current_pressure)
+                + "\n humidity (in percentage) = "
+                + str(self.current_humidity)
+                + "\n description = "
+                + str(self.weather_description)
+            )
+
+    def kelvin_to_celsius(self, kelvin):
+        return kelvin - 273.15
+
+    def kelvin_to_fahrenheit(self, kelvin):
+        return (kelvin - 273.15) * 9 / 5 + 32
+
 
 def main():
+    a = OpenWeatherMap()
+    a.update()
+    a.update()
 
-    # load the settings.json into a settings object
-    with open('settings.json') as json_file:
-        settings = json.load(json_file)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-    
