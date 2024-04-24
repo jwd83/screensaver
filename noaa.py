3546fd851d1478d7357702c82dc121587ad02e9b
Jared De Blander <jared@ayrscott.com>
2024-04-23 20:10:28 -0400

    work on noaa api

--- noaa.py
+++ noaa.py
@@ -1,6 +1,8 @@
-# let's talk to noaa!
+# Author: Jared De Blander (github.com/jwd83)
 #
-# the National Oceanic and Atmospheric Administration has a public api
+# Let's talk to National Oceanic and Atmospheric Administration! (NOAA)
+#
+# The National Oceanic and Atmospheric Administration has a public api
 # that we can use to get weather data and alerts
 #
 # default rate limit is 60 second cooldown
@@ -15,7 +17,14 @@ import json
 
 
 class NOAA:
-    def __init__(self, cooldown=60, verbose_enabled=False, lat=None, lon=None, logging_enabled=False):
+    def __init__(
+        self,
+        cooldown=60,
+        verbose_enabled=False,
+        lat=None,
+        lon=None,
+        logging_enabled=False,
+    ):
 
         with open("settings.json") as json_file:
             settings = json.load(json_file)
@@ -35,6 +44,7 @@ class NOAA:
         self.cooldown = int(cooldown)
         self.verbose_enabled = bool(verbose_enabled)
         self.logging_enabled = bool(logging_enabled)
+
         self.last_update = 0
         if self.cooldown < 1:
             self.cooldown = 1
@@ -101,10 +111,83 @@ class NOAA:
         with open("noaa-forecast-hourly.json", "w") as file:
             json.dump(self.forecast_hourly_data, file, indent=4)
 
-
     def verbose(self, str: str):
         if self.verbose_enabled:
             print(str)
+
+    def get_short_forecast(self):
+        if self.forecast_data is not None:
+            return self.forecast_data["properties"]["periods"][0]["shortForecast"]
+        else:
+            return "No Data"
+
+    def current_hourly_forecast(self):
+        if self.forecast_hourly_data is not None:
+            return self.get_active_periods()[0]
+        else:
+            return "No Data"
+
+    def get_instantaneous_temperature(self):
+        if self.forecast_hourly_data is not None:
+
+            # extract the minutes and seconds of the current time
+            current_time = time.localtime()
+            fractional_seconds = time.time() % 1
+
+            seconds_into_hour = (
+                current_time.tm_sec + current_time.tm_min * 60 + fractional_seconds
+            )
+            hour_progress = seconds_into_hour / (60 * 60)
+
+            # get the temperature for this hour and next hour
+            temperatures = self.get_hourly_temperatures()
+
+            t1 = temperatures[0]
+            t2 = temperatures[1]
+
+            # linear interpolation
+            ti = t1 + (t2 - t1) * hour_progress
+
+            return ti
+
+        else:
+            return 0
+
+    def get_active_periods(self):
+
+        active_periods = []
+
+        if self.forecast_hourly_data is not None:
+
+            # reject any data that is before the current time
+            current_time = time.time()
+
+            for period in self.forecast_hourly_data["properties"]["periods"]:
+                # look at when this hour ends
+                end_time = period["endTime"]
+
+                # end time is in the following format 2024-04-23T19:00:00-04:00
+                # we need to convert this to unix time
+
+                end_time_unix = time.mktime(
+                    time.strptime(end_time, "%Y-%m-%dT%H:%M:%S%z")
+                )
+
+                if current_time < end_time_unix:
+                    active_periods.append(period)
+
+        return active_periods
+
+    def get_hourly_temperatures(self):
+
+        hourly_temps = []
+
+        active_periods = self.get_active_periods()
+
+        for period in active_periods:
+            hourly_temps.append(period["temperature"])
+
+        return hourly_temps
 
 
 if __name__ == "__main__":
@@ -114,10 +197,16 @@ if __name__ == "__main__":
     print(noaa.forecast_data["properties"]["periods"][0]["detailedForecast"])
     print("Hourly Forecast")
 
-    for period in noaa.forecast_hourly_data["properties"]["periods"]:
-        print(
-            f"{period['startTime']} to {period['endTime']} {period['temperature']}F {period['shortForecast']}"
-        )
-    # loop through the periods
+    # for period in noaa.forecast_hourly_data["properties"]["periods"]:
+    #     print(
+    #         f"{period['startTime']} to {period['endTime']} {period['temperature']}F {period['shortForecast']}"
+    #     )
+    # # loop through the periods
+
+    print(noaa.get_hourly_temperatures())
+    print("Active periods")
+    print(noaa.get_active_periods())
+
+    print(noaa.get_instantaneous_temperature())
 
     noaa.update()
